21749ac962593641ef30b7ff7fbae97c74bfbdf4
Arsenii <shelahola@gmail.com>
2025-06-27 03:43:16 +0200

    add gray panel on starting screen

--- src/main/java/xyz/dream/launcher/MainScreen.java
+++ src/main/java/xyz/dream/launcher/MainScreen.java
@@ -7,10 +7,16 @@ public class MainScreen extends Application {
     public void start(javafx.stage.Stage primaryStage) {
         System.out.println("xyz.dream.launcher.MainScreen started successfully.");
 
-        // Window
+        Pane root = new Pane();
+        Scene scene = new Scene(root, 1040, 589);
+
+        // Background color
+        scene.setFill(javafx.scene.paint.Color.web("#1C1C1C"));
+        root.setStyle("-fx-background-color: #1C1C1C;");
+
+        // Application title
         primaryStage.setTitle("Dream Launcher");
-        primaryStage.setWidth(800);
-        primaryStage.setHeight(600);
+        primaryStage.setScene(scene);
         primaryStage.setResizable(false);
 
         // Show the main screen
