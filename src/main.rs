1077b8a07abac9db3c6600654243b0381450c995
Arsenii <cubelius@icloud.com>
2025-07-09 15:21:57 +0200

    change entry point and add styles.css

--- src/main.rs
+++ src/main.rs
@@ -1,10 +1,25 @@
-mod creeper;
+use dioxus::LaunchBuilder;
+use dioxus_desktop::{Config, LogicalSize, WindowBuilder};
 mod launcher;
+mod creeper;
 
-fn main() {
-    // Backend
+pub fn main() {
+    let size = LogicalSize::new(1280.0, 832.0);
+
+    let config = Config::default().with_window(
+        WindowBuilder::new()
+            .with_title("Dream Launcher")
+            .with_inner_size(size)
+            .with_min_inner_size(size)
+            .with_max_inner_size(size)
+            .with_resizable(false)
+    );
+
+    LaunchBuilder::new()
+        .with_cfg(config)
+        .launch(launcher::launcher::launcher);
+}
+
+fn call_creeper() {
     // let _ = creeper::creeper::main();
-
-    // Frontend
-    let _ = launcher::launcher::main();
 }
